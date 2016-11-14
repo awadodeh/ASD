f5468121dcb5e661978752a2353d020a8b9d3527
awadodeh <awadodeh@gmail.com>
2016-11-14 15:26:58 -0600

    addin class diagram

--- statePattern/src/main/java/statePattern_after/CeilingFan.java
+++ statePattern/src/main/java/statePattern_after/CeilingFan.java
@@ -20,39 +20,12 @@ public class CeilingFan {
 		
 	}
 	
-	public void pullgreen() {
-//		if (state == 0) {
-//			state = 1;
-//			System.out.println( "low speed" );
-//		} else if (state == 1) {
-//			state = 2;
-//			System.out.println( "medium speed" );
-//		} else if (state == 2) {
-//			state = 3;
-//			System.out.println( "high speed" );
-//		} else {
-//			state = 0;
-//			System.out.println( "turning off" );
-//		}
-		
+	public void pullgreen() {		
 		state.pullgreen();
 	}
 
 	public void pullred() {
-//		if (state == 0) {
-//			state = 3;
-//			System.out.println( "high speed" );
-//		} else if (state == 1) {
-//			state = 0;
-//			System.out.println( "turning off" );
-//		} else if (state == 2) {
-//			state = 1;
-//			System.out.println( "low speed" );
-//		} else {
-//			state = 2;
-//			System.out.println( "medium speed" );
-//		}
-		
+
 		state.pullred();
 	} 
 
